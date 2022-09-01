3003f08e67b7a67ccb0d46d6c1e14b25e4c8633e
B.J. Self <bj.self@wwt.com>
2022-09-01 17:01:43 -0500

    Paper texture

--- src/app/data/initialPosts.ts
+++ src/app/data/initialPosts.ts
@@ -17,5 +17,45 @@ export const POSTS: Post[] = [
         I go into my first job at the Veejay Hotel in Baltimore, Maryland.`,
         postDate: new Date("8-1-80"),
         rotation: 0.25
+      },
+      {
+        postTitle: "My Shawl",
+        postText: `I went back to 1865 for my shawl!
+
+        Oh, I was so happy, I picked out the border that I fell in love with at 11: 00 last night and headed off to bed.
+        
+        I needed to be up early in the morning to go to our pumpkin carving party, which went well.
+        
+        The pumpkin pieces were a little more difficult to carve than usual, though they turned out okay.
+        
+        Some of them looked more like the ghosts I wanted.
+        
+        If I try again, I'll have to make them look more like they're supposed to be.
+        
+        After Halloween, there are many houseguests coming.
+        
+        I won't have time to work on it.
+        
+        But I'll try not to drop my knitting.`,
+        postDate: new Date("1-4-83"),
+        rotation: -0.17
+      },
+      {
+        postTitle: "Favorite Story",
+        postText: `My favorite story from my travels through time and space.
+
+        I wanted to write this story because when I was a little girl, my parents read me Jan de Hartog's "One false move" and "And the earth moved" over and over.
+        
+        My parents said that the best childhood stories should always have a happy ending.
+        
+        This one did, for me at least.
+        
+        Oh, if they only knew how angry I am that the book is actually called "Two gone bad" by now.
+        
+        Never saw that one coming.`,
+        postDate: new Date("June 12, 2014"),
+        rotation: -0.17
       }
+
+      
 ]
