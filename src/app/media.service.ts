c793984c1c35a1980c6d5d93a8c8a6a4b861b4f8
B.J. Self <bj.self@wwt.com>
2022-09-09 12:57:14 -0500

    Local storage working for media. BJS

--- src/app/media.service.ts
+++ src/app/media.service.ts
@@ -24,21 +24,24 @@ export class MediaService {
 
     // get random image url
     const randomImageUrl = "https://loremflickr.com/640/480/time%20travel?" + randomNumber.toString()
-    
+
     const randomRotation = Math.random() * (rotationScale - (0 - rotationScale)) + (0 - rotationScale);
     const newMediaList = [...this.mediaSubject.getValue(), {...newMedia, rotation: randomRotation, mediaUrl: randomImageUrl}];
+    console.log("Storing Media List", JSON.stringify(newMediaList));
+    localStorage.setItem('mediaList', JSON.stringify(newMediaList));
     this.mediaSubject.next(newMediaList);
   }
 
   constructor() {
     const storedMediaList = localStorage.getItem('mediaList');
+    console.log("Stored Media", storedMediaList);
     if ( storedMediaList === null) {
       this.mediaSubject = new BehaviorSubject(MEDIA);
       this.mediaAction = this.mediaSubject.asObservable();
     } else {
-      let mediaList = JSON.parse(storedMediaList).map((media) => {
-        media.postDate = new Date(media.postDate);
-      });
+      let mediaList = JSON.parse(storedMediaList);
+      mediaList.map((media) => {media.mediaDate = new Date(media.mediaDate)});
+      console.log("Parsed Media List", mediaList);
       this.mediaSubject = new BehaviorSubject(mediaList);
       this.mediaAction = this.mediaSubject.asObservable();
     }
