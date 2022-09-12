c3347c79a041a74a3cb1e4fba18ff2ff175870d1
B.J. Self <bj.self@wwt.com>
2022-09-12 16:49:48 -0500

    tweaked various things to get ready to present. BJS

--- src/app/data/initialMedia.ts
+++ src/app/data/initialMedia.ts
@@ -2,16 +2,16 @@ import { Media } from "../media";
 
 export const MEDIA: Media[] = [
     {
-        mediaTitle: "Photo One",
-        mediaAltText: "Photo One",
+        mediaTitle: "Me in the Desert",
+        mediaAltText: "Me walking in the desert in my jacket",
         mediaUrl: "../assets/images/photos/jacket_photo.jpg",
         mediaDate: new Date(),
         rotation: 0.56
     },
     {
-        mediaTitle: "Photo Two",
-        mediaAltText: "Photo Two",
-        mediaUrl: "../assets/images/photos/jacket_photo.jpg",
+        mediaTitle: "Sunset AD1682",
+        mediaAltText: "Photo of the ocean at sunset from my trip to AD1682",
+        mediaUrl: "../assets/images/photos/photo_two.jpg",
         mediaDate: new Date(),
         rotation: -0.32
     }
